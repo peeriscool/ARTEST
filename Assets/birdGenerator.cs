94621f80da9498e9f1fa894ba7e8e2430be60037
tatumvt <62714286+tatumvt@users.noreply.github.com>
2021-03-25 12:01:55 +0100

    gitignore change

---
+++ Assets/birdGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birdGenerator : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public GameObject spawnPos;
+    public bool stopSpawning;
+    public float spawnTime;
+    public float spawnDelay;
+
+    public bool isBird;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+        stopSpawning = false;
+    }
+
+    public void SpawnObject()
+    {
+        if (isBird)
+        {
+            Instantiate(objectToSpawn, spawnPos.transform.position, transform.rotation);
+            spawnDelay = Random.Range(2, 10);
+            if (stopSpawning)
+            {
+                CancelInvoke("SpawnObject");
+            }
+        }
+        else
+        {
+            Instantiate(objectToSpawn, new Vector2(spawnPos.transform.position.x, -0.8f), transform.rotation);
+            spawnDelay = Random.Range(2, 10);
+            if (stopSpawning)
+            {
+                CancelInvoke("SpawnObject");
+            }
+        }
+    }
+}
